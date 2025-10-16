14f98844fecaf6b0e1d874044c737667fe73bd64
Daniel Borja <danielborjac@hotmail.es>
2025-10-16 12:46:36 -0500

    correcciónes diseño y se agregó la nueva sección alianzas académicas

--- front/src/components/AcademicAlliances.jsx
+++ front/src/components/AcademicAlliances.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
-import "./academicAlliances.css";
+import "./AcademicAlliances.css";
 import { motion } from "framer-motion";
 import { useNavigate } from "react-router-dom";
 import { fadeInUp, hover3D, slideRotateLeft} from "../utils/motionConfig";
